9c5fd7087664ca2a41d79abc8e20f8892934c585
Adil Asselman <aasselma@c2r9p2.1337.ma>
2024-06-06 20:45:01 +0100

    ....

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # smalloc
 this repo explores the inner workings of memory allocation by implementing a small version of the malloc function in C. in this project i'm aiming to get a deep understanding of how the operating system manages memory and the concepts behind dynamic memory allocation.
+
--- smalloc.c
+++ smalloc.c
@@ -2,8 +2,8 @@
 
 void    *smalloc(int size)
 {
-  void *p = sbrk(0);
-  void *request = sbrk(size);
+  void *p = sbrk(0); // when we give the number 0 to sbrk it return the adress of the current top of the heap
+  void *request = sbrk(size); // and here we increment the heap size by "size"
   if (request == (void*) -1) {
     return NULL; // sbrk failed.
   } else {
